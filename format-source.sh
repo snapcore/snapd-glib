7d36595a905f3fe601a45c5c2905f1957495dee6
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2024-06-13 12:03:20 +0200

    Added CI test

--- format-source.sh
+++ format-source.sh
@@ -1,6 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
+passed=true
 for file in snapd-glib/*.[ch] snapd-glib/requests/*.[ch] snapd-qt/*.cpp snapd-qt/*.h snapd-qt/Snapd/*.h tests/*.[ch] tests/*.cpp; do
-    echo Formating $file
-    clang-format -i $file
+    if [ $# -eq 0 ];
+        # no parameters? Just apply the changes
+        echo Formating $file
+        clang-format -i $file
+    else
+        # any parameter? check that the formatting is fine
+        clang-format -i $file > $file.formatted
+        echo $file
+        diff $file $file.formatted
+        if [ $? != 0 ]; then
+            passed=false
+        fi
+        rm $file.formatted
+    fi
 done
+if [ $passed = false ]; then
+    exit 1
+fi
